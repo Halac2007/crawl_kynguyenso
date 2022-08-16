2fb18ee77fa34925777ef33efd11d66b12e06a8c
Be Mai <bemai@gmail.com>
2022-08-16 09:35:37 +0700

    get data

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ app.get('/', async (req, res) => {
       const html = response.body
       const $ = cheerio.load(html)
       const articles = []
-      $('.story', html).each(function () {
+      $('.rank-1', html).each(function () {
         // const title = $(this).text().trim()
-        const title = $(this).find('a').attr('title')
-        const link = $(this).find('a').attr('href')
-        const imageTop = $(this).find('a').find('img').attr('src')
+        const title = $(this).find('.story').find('a').attr('title')
+        const link = $(this).find('.story').find('a').attr('href')
+        const imageTop = $(this).find('.story').find('a').find('img').attr('src')
         const image =
-          $(this).find('a').find('img').attr('data-src') ||
+          $(this).find('.story').find('a').find('img').attr('data-src') ||
           'https://photo-cms-plo.zadn.vn/600x360/Uploaded/2022/pwvotwiv/2022_05_30/4-dad4-451.jpg'
         const time = $(this).find('.story__time').text().trim() || ' '
         articles.push({
