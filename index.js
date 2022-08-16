b05fb8885825d3e21274cfb194428f1f891a6814
Be Mai <bemai@gmail.com>
2022-08-16 20:43:37 +0700

    update data

--- index.js
+++ index.js
@@ -36,6 +36,35 @@ app.get('/', async (req, res) => {
     })
 })
 
+app.get('/nhip-cong-nghe', async (req, res) => {
+  got('https://kynguyenso.plo.vn/ky-nguyen-so/')
+    .then((response) => {
+      const html = response.body
+      const $ = cheerio.load(html)
+      const articles = []
+      $('.story', html).each(function () {
+        // const title = $(this).text().trim()
+        const title = $(this).find('a').attr('title')
+        const link = $(this).find('a').attr('href')
+        const imageTop = $(this).find('a').find('img').attr('src')
+        const image =
+          $(this).find('a').find('img').attr('data-src') ||
+          'https://photo-cms-plo.zadn.vn/600x360/Uploaded/2022/pwvotwiv/2022_05_30/4-dad4-451.jpg'
+        const time = $(this).find('.story__time').text().trim() || ' '
+        articles.push({
+          title,
+          link,
+          imageTop,
+          image,
+          time,
+        })
+      })
+      res.json(articles)
+    })
+    .catch((err) => {
+      console.log('Error: ', err.message)
+    })
+})
 app.get('/thiet-bi-so', async (req, res) => {
   got('https://kynguyenso.plo.vn/ky-nguyen-so/thiet-bi-so/')
     .then((response) => {
