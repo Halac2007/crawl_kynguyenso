46f75780814dd861bc857f63cda818e57e66b8d4
Be Mai <bemai@gmail.com>
2022-08-16 14:20:09 +0700

    sum data

--- index.js
+++ index.js
@@ -7,6 +7,66 @@ const app = express()
 app.use(cors())
 
 app.get('/', async (req, res) => {
+  got('https://kynguyenso.plo.vn/ky-nguyen-so/nhip-cong-nghe/')
+    .then((response) => {
+      const html = response.body
+      const $ = cheerio.load(html)
+      const articles = []
+      $('.story', html).each(function () {
+        // const title = $(this).text().trim()
+        const title = $(this).find('a').attr('title')
+        const link = $(this).find('a').attr('href')
+        const imageTop = $(this).find('a').find('img').attr('src')
+        const image =
+          $(this).find('a').find('img').attr('data-src') ||
+          'https://photo-cms-plo.zadn.vn/600x360/Uploaded/2022/pwvotwiv/2022_05_30/4-dad4-451.jpg'
+        const time = $(this).find('.story__time').text().trim() || ' '
+        articles.push({
+          title,
+          link,
+          imageTop,
+          image,
+          time,
+        })
+      })
+      res.json(articles)
+    })
+    .catch((err) => {
+      console.log('Error: ', err.message)
+    })
+})
+
+app.get('/thiet-bi-so', async (req, res) => {
+  got('https://kynguyenso.plo.vn/ky-nguyen-so/thiet-bi-so/')
+    .then((response) => {
+      const html = response.body
+      const $ = cheerio.load(html)
+      const articles = []
+      $('.story', html).each(function () {
+        // const title = $(this).text().trim()
+        const title = $(this).find('a').attr('title')
+        const link = $(this).find('a').attr('href')
+        const imageTop = $(this).find('a').find('img').attr('src')
+        const image =
+          $(this).find('a').find('img').attr('data-src') ||
+          'https://photo-cms-plo.zadn.vn/600x360/Uploaded/2022/pwvotwiv/2022_05_30/4-dad4-451.jpg'
+        const time = $(this).find('.story__time').text().trim() || ' '
+        articles.push({
+          title,
+          link,
+          imageTop,
+          image,
+          time,
+        })
+      })
+      res.json(articles)
+    })
+    .catch((err) => {
+      console.log('Error: ', err.message)
+    })
+})
+
+app.get('/tuyet-chieu', async (req, res) => {
   got('https://kynguyenso.plo.vn/ky-nguyen-so/tuyet-chieu/')
     .then((response) => {
       const html = response.body
@@ -18,7 +78,97 @@ app.get('/', async (req, res) => {
         const link = $(this).find('a').attr('href')
         const imageTop = $(this).find('a').find('img').attr('src')
         const image =
-          $(this).find('.story').find('a').find('img').attr('data-src') ||
+          $(this).find('a').find('img').attr('data-src') ||
+          'https://photo-cms-plo.zadn.vn/600x360/Uploaded/2022/pwvotwiv/2022_05_30/4-dad4-451.jpg'
+        const time = $(this).find('.story__time').text().trim() || ' '
+        articles.push({
+          title,
+          link,
+          imageTop,
+          image,
+          time,
+        })
+      })
+      res.json(articles)
+    })
+    .catch((err) => {
+      console.log('Error: ', err.message)
+    })
+})
+
+app.get('/kinh-doanh-online', async (req, res) => {
+  got('https://kynguyenso.plo.vn/ky-nguyen-so/kinhdoanhonline/')
+    .then((response) => {
+      const html = response.body
+      const $ = cheerio.load(html)
+      const articles = []
+      $('.story', html).each(function () {
+        // const title = $(this).text().trim()
+        const title = $(this).find('a').attr('title')
+        const link = $(this).find('a').attr('href')
+        const imageTop = $(this).find('a').find('img').attr('src')
+        const image =
+          $(this).find('a').find('img').attr('data-src') ||
+          'https://photo-cms-plo.zadn.vn/600x360/Uploaded/2022/pwvotwiv/2022_05_30/4-dad4-451.jpg'
+        const time = $(this).find('.story__time').text().trim() || ' '
+        articles.push({
+          title,
+          link,
+          imageTop,
+          image,
+          time,
+        })
+      })
+      res.json(articles)
+    })
+    .catch((err) => {
+      console.log('Error: ', err.message)
+    })
+})
+
+app.get('/an-sach-song-khoe', async (req, res) => {
+  got('https://plo.vn/an-sach-song-khoe/')
+    .then((response) => {
+      const html = response.body
+      const $ = cheerio.load(html)
+      const articles = []
+      $('.story', html).each(function () {
+        // const title = $(this).text().trim()
+        const title = $(this).find('a').attr('title')
+        const link = $(this).find('a').attr('href')
+        const imageTop = $(this).find('a').find('img').attr('src')
+        const image =
+          $(this).find('a').find('img').attr('data-src') ||
+          'https://photo-cms-plo.zadn.vn/600x360/Uploaded/2022/pwvotwiv/2022_05_30/4-dad4-451.jpg'
+        const time = $(this).find('.story__time').text().trim() || ' '
+        articles.push({
+          title,
+          link,
+          imageTop,
+          image,
+          time,
+        })
+      })
+      res.json(articles)
+    })
+    .catch((err) => {
+      console.log('Error: ', err.message)
+    })
+})
+
+app.get('/xe-va-luat', async (req, res) => {
+  got('https://plo.vn/xe-va-luat/')
+    .then((response) => {
+      const html = response.body
+      const $ = cheerio.load(html)
+      const articles = []
+      $('.story', html).each(function () {
+        // const title = $(this).text().trim()
+        const title = $(this).find('a').attr('title')
+        const link = $(this).find('a').attr('href')
+        const imageTop = $(this).find('a').find('img').attr('src')
+        const image =
+          $(this).find('a').find('img').attr('data-src') ||
           'https://photo-cms-plo.zadn.vn/600x360/Uploaded/2022/pwvotwiv/2022_05_30/4-dad4-451.jpg'
         const time = $(this).find('.story__time').text().trim() || ' '
         articles.push({
