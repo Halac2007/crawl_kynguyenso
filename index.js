159b4509027629c49db65dd46559f13fcceb7855
Be Mai <bemai@gmail.com>
2022-08-26 15:56:46 +0700

    edit data

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ import axios from 'axios'
 const app = express()
 app.use(cors())
 
-app.get('/', async (req, res) => {
-  got('https://kynguyenso.plo.vn/ky-nguyen-so/')
+app.get('/cong-nghe-moi', async (req, res) => {
+  got('https://kynguyenso.plo.vn/')
     .then((response) => {
       const html = response.body
       const $ = cheerio.load(html)
       const articles = []
-      $('.story', html).each(function () {
+      $('.rank-1', html).each(function () {
         // const title = $(this).text().trim()
-        const title = $(this).find('a').attr('title')
-        const link = $(this).find('a').attr('href')
-        const image = $(this).find('a').find('img').attr('data-src') || $(this).find('a').find('img').attr('src')
+        const title = $(this).find('.story').find('a').attr('title')
+        const link = $(this).find('.story').find('a').attr('href')
+        const image = $(this).find('.story').find('a').find('img').attr('data-src') || $(this).find('a').find('img').attr('src')
 
-        const time = $(this).find('.story__time').text().trim() || ' '
+        const time = $(this).find('.story').find('.story__time').text().trim() || ' '
         articles.push({
           title,
           link,
@@ -34,7 +34,7 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/nhip-cong-nghe', async (req, res) => {
-  got('https://kynguyenso.plo.vn/ky-nguyen-so/nhip-cong-nghe')
+  got('https://kynguyenso.plo.vn/ky-nguyen-so/nhip-cong-nghe/')
     .then((response) => {
       const html = response.body
       const $ = cheerio.load(html)
@@ -54,7 +54,9 @@ app.get('/nhip-cong-nghe', async (req, res) => {
           time,
         })
       })
-      res.json(articles)
+
+      const newarr = articles.splice(1, 3)
+      res.json(articles.slice(newarr))
     })
     .catch((err) => {
       console.log('Error: ', err.message)
@@ -80,7 +82,8 @@ app.get('/thiet-bi-so', async (req, res) => {
           time,
         })
       })
-      res.json(articles)
+      const newarr = articles.splice(1, 3)
+      res.json(articles.slice(newarr))
     })
     .catch((err) => {
       console.log('Error: ', err.message)
@@ -107,7 +110,8 @@ app.get('/tuyet-chieu', async (req, res) => {
           time,
         })
       })
-      res.json(articles)
+      const newarr = articles.splice(1, 3)
+      res.json(articles.slice(newarr))
     })
     .catch((err) => {
       console.log('Error: ', err.message)
@@ -136,7 +140,8 @@ app.get('/kinh-doanh-online', async (req, res) => {
           time,
         })
       })
-      res.json(articles)
+      const newarr = articles.splice(1, 3)
+      res.json(articles.slice(newarr))
     })
     .catch((err) => {
       console.log('Error: ', err.message)
@@ -163,7 +168,8 @@ app.get('/cong-nghe-40', async (req, res) => {
           time,
         })
       })
-      res.json(articles)
+      const newarr = articles.splice(1, 3)
+      res.json(articles.slice(newarr))
     })
     .catch((err) => {
       console.log('Error: ', err.message)
@@ -175,6 +181,7 @@ app.get('/an-sach-song-khoe', async (req, res) => {
     .then((response) => {
       const html = response.body
       const $ = cheerio.load(html)
+
       const articles = []
       $('.story', html).each(function () {
         // const title = $(this).text().trim()
@@ -191,7 +198,17 @@ app.get('/an-sach-song-khoe', async (req, res) => {
           time,
         })
       })
-      res.json(articles)
+
+      const arrNew = articles.slice(4)
+
+      const arrNewC = arrNew.splice(15, 23)
+
+      const arrNewSpliceEnd = arrNew.splice(arrNewC)
+
+      const arrEnd = arrNewSpliceEnd.splice(19)
+
+      // const newarr = articles.splice(15, 27)
+      res.json(arrNewSpliceEnd.splice(arrEnd))
     })
     .catch((err) => {
       console.log('Error: ', err.message)
@@ -219,16 +236,85 @@ app.get('/xe-va-luat', async (req, res) => {
         })
       })
 
-      res.json(articles)
+      const arrNew = articles.slice(4)
+
+      const arrNewC = arrNew.splice(15, 23)
+
+      const arrNewSpliceEnd = arrNew.splice(arrNewC)
+
+      const arrEnd = arrNewSpliceEnd.splice(19)
+
+      // const newarr = articles.splice(15, 27)
+      res.json(arrNewSpliceEnd.splice(arrEnd))
     })
     .catch((err) => {
       console.log('Error: ', err.message)
     })
 })
 
-/*-----------------gộp data------------------------*/
+/*-------------phân trang-------------------------*/
 
-app.get('/tong-hop', async (req, res) => {
+app.get('/an-sach-song-khoe/page1', async (req, res) => {
+  got('https://plo.vn/an-sach-song-khoe/?trang=2')
+    .then((response) => {
+      const html = response.body
+      const $ = cheerio.load(html)
+      const articles = []
+      $('.story', html).each(function () {
+        // const title = $(this).text().trim()
+        const title = $(this).find('a').attr('title')
+        const link = $(this).find('a').attr('href')
+        const image = $(this).find('a').find('img').attr('data-src') || $(this).find('a').find('img').attr('src')
+        const time = $(this).find('.story__time').text().trim() || ' '
+        articles.push({
+          title,
+          link,
+
+          image,
+          time,
+        })
+      })
+
+      res.json(articles.slice(4))
+    })
+    .catch((err) => {
+      console.log('Error: ', err.message)
+    })
+})
+
+/*------------------*/
+
+app.get('/xe-va-luat/page1', async (req, res) => {
+  got('https://plo.vn/xe-va-luat/?trang=2')
+    .then((response) => {
+      const html = response.body
+      const $ = cheerio.load(html)
+      const articles = []
+      $('.story', html).each(function () {
+        // const title = $(this).text().trim()
+        const title = $(this).find('a').attr('title')
+        const link = $(this).find('a').attr('href')
+        const image = $(this).find('a').find('img').attr('data-src') || $(this).find('a').find('img').attr('src')
+        const time = $(this).find('.story__time').text().trim() || ' '
+        articles.push({
+          title,
+          link,
+
+          image,
+          time,
+        })
+      })
+
+      res.json(articles.slice(4))
+    })
+    .catch((err) => {
+      console.log('Error: ', err.message)
+    })
+})
+
+/*-----------phân trang page 1------------*/
+
+app.get('/ky-nguyen-so/page1', async (req, res) => {
   const respRepos = await axios(`https://kynguyenso.herokuapp.com/nhip-cong-nghe`)
   const res1 = await axios(`https://kynguyenso.herokuapp.com/cong-nghe-40`)
   const res2 = await axios(`https://kynguyenso.herokuapp.com/tuyet-chieu`)
@@ -247,6 +333,27 @@ app.get('/tong-hop', async (req, res) => {
   res.send(arrNews)
 })
 
+/*-----------------gộp data------------------------*/
+
+app.get('/tong-hop', async (req, res) => {
+  const respRepos = await axios(`https://kynguyenso.herokuapp.com/nhip-cong-nghe`)
+  const res1 = await axios(`https://kynguyenso.herokuapp.com/cong-nghe-40`)
+  const res2 = await axios(`https://kynguyenso.herokuapp.com/tuyet-chieu`)
+  const res3 = await axios(`https://kynguyenso.herokuapp.com/thiet-bi-so`)
+  const res4 = await axios(`https://kynguyenso.herokuapp.com/kinh-doanh-online`)
+  const res5 = await axios(`https://kynguyenso.herokuapp.com/`)
+
+  const arr1 = respRepos.data
+  const arr2 = res1.data
+  const arr3 = res2.data
+  const arr4 = res3.data
+  const arr5 = res4.data
+  const arr = res5.data
+
+  const arrNews = [...arr, ...arr1, ...arr2, ...arr3, ...arr4, ...arr5]
+  res.send(arrNews)
+})
+
 /*-------------------------*/
 
 app.get('/ky-nguyen-so', async (req, res) => {
@@ -255,16 +362,14 @@ app.get('/ky-nguyen-so', async (req, res) => {
   const res2 = await axios(`https://kynguyenso.herokuapp.com/tuyet-chieu`)
   const res3 = await axios(`https://kynguyenso.herokuapp.com/thiet-bi-so`)
   const res4 = await axios(`https://kynguyenso.herokuapp.com/kinh-doanh-online`)
-  const res5 = await axios(`https://kynguyenso.herokuapp.com/`)
 
-  const arr1 = respRepos.data.slice(0, 4)
-  const arr2 = res1.data.slice(0, 4)
-  const arr3 = res2.data.slice(0, 4)
-  const arr4 = res3.data.slice(0, 4)
-  const arr5 = res4.data.slice(0, 4)
-  const arr = res5.data.slice(0, 4)
+  const arr1 = respRepos.data
+  const arr2 = res1.data
+  const arr3 = res2.data
+  const arr4 = res3.data
+  const arr5 = res4.data
 
-  const arrNews = [...arr, ...arr1, ...arr2, ...arr3, ...arr4, ...arr5]
+  const arrNews = [...arr1, ...arr2, ...arr3, ...arr4, ...arr5]
   res.send(arrNews)
 })
 
