633a2c8ffdb35698364f07a5ce3999e52433a58c
Be Mai <bemai@gmail.com>
2022-08-26 16:11:01 +0700

    cut data splice

--- index.js
+++ index.js
@@ -335,53 +335,54 @@ app.get('/ky-nguyen-so/page1', async (req, res) => {
 
 /*-----------------gộp data------------------------*/
 
-// app.get('/tong-hop', async (req, res) => {
-//   const respRepos = await axios(`https://kynguyenso.herokuapp.com/nhip-cong-nghe`)
-//   const res1 = await axios(`https://kynguyenso.herokuapp.com/cong-nghe-40`)
-//   const res2 = await axios(`https://kynguyenso.herokuapp.com/tuyet-chieu`)
-//   const res3 = await axios(`https://kynguyenso.herokuapp.com/thiet-bi-so`)
-//   const res4 = await axios(`https://kynguyenso.herokuapp.com/kinh-doanh-online`)
-//   const res5 = await axios(`https://kynguyenso.herokuapp.com/`)
+app.get('/tong-hop', async (req, res) => {
+  const respRepos = await axios(`https://kynguyenso.herokuapp.com/nhip-cong-nghe`)
+  const res1 = await axios(`https://kynguyenso.herokuapp.com/cong-nghe-40`)
+  const res2 = await axios(`https://kynguyenso.herokuapp.com/tuyet-chieu`)
+  const res3 = await axios(`https://kynguyenso.herokuapp.com/thiet-bi-so`)
+  const res4 = await axios(`https://kynguyenso.herokuapp.com/kinh-doanh-online`)
+  const res5 = await axios(`https://kynguyenso.herokuapp.com/`)
 
-//   const arr1 = respRepos.data
-//   const arr2 = res1.data
-//   const arr3 = res2.data
-//   const arr4 = res3.data
-//   const arr5 = res4.data
-//   const arr = res5.data
+  const arr1 = respRepos.data
+  const arr2 = res1.data
+  const arr3 = res2.data
+  const arr4 = res3.data
+  const arr5 = res4.data
+  const arr = res5.data
 
-//   const arrNews = [...arr, ...arr1, ...arr2, ...arr3, ...arr4, ...arr5]
-//   res.send(arrNews)
-// })
+  const arrNews = [...arr, ...arr1, ...arr2, ...arr3, ...arr4, ...arr5]
+
+  res.send(arrNews)
+})
 
 /*-------------------------*/
 
-// app.get('/ky-nguyen-so', async (req, res) => {
-//   const respRepos = await axios(`https://kynguyenso.herokuapp.com/nhip-cong-nghe`)
-//   const res1 = await axios(`https://kynguyenso.herokuapp.com/cong-nghe-40`)
-//   const res2 = await axios(`https://kynguyenso.herokuapp.com/tuyet-chieu`)
-//   const res3 = await axios(`https://kynguyenso.herokuapp.com/thiet-bi-so`)
-//   const res4 = await axios(`https://kynguyenso.herokuapp.com/kinh-doanh-online`)
+app.get('/ky-nguyen-so', async (req, res) => {
+  const respRepos = await axios(`https://kynguyenso.herokuapp.com/nhip-cong-nghe`)
+  const res1 = await axios(`https://kynguyenso.herokuapp.com/cong-nghe-40`)
+  const res2 = await axios(`https://kynguyenso.herokuapp.com/tuyet-chieu`)
+  const res3 = await axios(`https://kynguyenso.herokuapp.com/thiet-bi-so`)
+  const res4 = await axios(`https://kynguyenso.herokuapp.com/kinh-doanh-online`)
 
-//   const arr1 = respRepos.data
-//   const arr2 = res1.data
-//   const arr3 = res2.data
-//   const arr4 = res3.data
-//   const arr5 = res4.data
+  const arr1 = respRepos.data
+  const arr2 = res1.data
+  const arr3 = res2.data
+  const arr4 = res3.data
+  const arr5 = res4.data
 
-//   const arrNews = [...arr1, ...arr2, ...arr3, ...arr4, ...arr5]
-//   res.send(arrNews)
-// })
+  const arrNews = [...arr1, ...arr2, ...arr3, ...arr4, ...arr5]
+  res.send(arrNews)
+})
 
-// app.get('/tin-noi-bat', async (req, res) => {
-//   const respGlobal = await axios(`https://kynguyenso.herokuapp.com/`)
-//   const respRepos = await axios(`https://kynguyenso.herokuapp.com/xe-va-luat`)
-//   const arr1 = respGlobal.data.slice(0, 4)
-// //   const arr2 = respRepos.data.slice(8, 10)
-// //   const arr3 = [...arr1, ...arr2]
+app.get('/tin-noi-bat', async (req, res) => {
+  const respGlobal = await axios(`https://kynguyenso.herokuapp.com/`)
+  const respRepos = await axios(`https://kynguyenso.herokuapp.com/xe-va-luat`)
+  const arr1 = respGlobal.data
+  const arr2 = respRepos.data
+  const arr3 = [...arr1, ...arr2]
 
-// //   res.json(arr3)
-// })
+  res.json(arr3)
+})
 
 const port = process.env.PORT || 4000
 app.listen(port, () => {
